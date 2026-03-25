6b5fa3a351ca1f5498ab78f16ac3d56564999c82
giorgi473 <giorgi.kavtaradze2000@mail.ru>
2026-03-25 11:50:47 +0200

    Improve history cards layout and actions

--- app/RecordContext.tsx
+++ app/RecordContext.tsx
@@ -10,6 +10,7 @@ export type RecordItem = {
 type RecordContextType = {
   records: RecordItem[];
   addRecord: (item: Omit<RecordItem, "id">) => RecordItem;
+  deleteRecord: (id: number) => void;
 };
 
 const RecordContext = createContext<RecordContextType | undefined>(undefined);
@@ -23,8 +24,12 @@ export function RecordProvider({ children }: { children: React.ReactNode }) {
     return newRecord;
   };
 
+  const deleteRecord = (id: number) => {
+    setRecords((prev) => prev.filter((r) => r.id !== id));
+  };
+
   return (
-    <RecordContext.Provider value={{ records, addRecord }}>
+    <RecordContext.Provider value={{ records, addRecord, deleteRecord }}>
       {children}
     </RecordContext.Provider>
   );
